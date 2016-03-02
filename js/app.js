c91f9c41b273ab176421b74a70b7ccc5feab4f48
Matt Brock <mbrock8@gmail.com>
2016-03-02 11:54:25 -0600

    First commit for new responsive FSD Schedule

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// FSD Schedule Application
+
+// Build class listing constructor
+	// Day
+	// Studio
+	// Name of class
+	// Type of class
+	// Start time
+		// hours
+		// minutes
+	// End time
+		// hours
+		// minutes
+
